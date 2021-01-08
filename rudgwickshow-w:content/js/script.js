a572886fc0913978f761affe3d9f3460f9adccbd
Nathanael Pifer <npifer@Nathanaels-Air.home>
2021-01-08 12:29:41 -0500

    added to js

--- rudgwickshow-w:content/js/script.js
+++ rudgwickshow-w:content/js/script.js
@@ -1,39 +1,47 @@
 let event1Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 1";
+    document.querySelector('.event-image').setAttribute('src', 'img/steam.jpg');
+    document.querySelector('.event-text').innerHTML = "We have lots of steam powered machines on display for you to see. Including Tractors and many others.";
 }
 document.querySelector("#event-img-1").addEventListener("click", event1Details);
 
 let event2Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 2";
+    document.querySelector('.event-image').setAttribute('src', 'img/animal.jpg');
+    document.querySelector('.event-text').innerHTML = "There are lots of animals for you to see and pet. A great activity for your kids and grandkids to enjoy.";
 }
 document.querySelector("#event-img-2").addEventListener("click", event2Details);
 
 let event3Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 3";
+    document.querySelector('.event-image').setAttribute('src', 'img/fun.jpg');
+    document.querySelector('.event-text').innerHTML = "Some of our traditional events include: Horse Carrage Rides, etc...";
 }
 document.querySelector("#event-img-3").addEventListener("click", event3Details);
 
 let event4Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 4";
+    document.querySelector('.event-image').setAttribute('src', 'img/beer.jpg');
+    document.querySelector('.event-text').innerHTML = "Our annual beer festival is one of our largest events come out for the best craft beer you can find.";
 }
 document.querySelector("#event-img-4").addEventListener("click", event4Details);
 
 let event5Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 5";
+    document.querySelector('.event-image').setAttribute('src', 'img/drama.jpg');
+    document.querySelector('.event-text').innerHTML = "Come enjoy one of our Drama Performances. They are fun for the whole family.";
 }
 document.querySelector("#event-img-5").addEventListener("click", event5Details);
 
 let event6Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 6";
+    document.querySelector('.event-image').setAttribute('src', 'img/activity.jpg');
+    document.querySelector('.event-text').innerHTML = "There is plenty of music for the whole family to enjoy for traditional tunes to folk and much more.";
 }
 document.querySelector("#event-img-6").addEventListener("click", event6Details);
 
 let event7Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 7";
+    document.querySelector('.event-image').setAttribute('src', 'working-img/food.jpg');
+    document.querySelector('.event-text').innerHTML = "We have tons of food vendors so there is something for everyone to enjoy";
 }
 document.querySelector("#event-img-7").addEventListener("click", event7Details);
 
 let event8Details = () => {
-    document.querySelector('.event-text').innerHTML = "Details About Event 8";
+    document.querySelector('.event-image').setAttribute('src', 'img/ride.jpg');
+    document.querySelector('.event-text').innerHTML = "You can take a ride on a tractor around the fair. It is a great experience for the kids.";
 }
 document.querySelector("#event-img-8").addEventListener("click", event8Details);
